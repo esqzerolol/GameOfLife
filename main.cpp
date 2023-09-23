c1bca7ba3d248b32bac3fb1dd36b431e7e3123a7
esqzerolol <68171967+esqzerolol@users.noreply.github.com>
2023-09-23 18:11:34 -0300

    GridGrafico criado

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
+#include "raylib.h"
 #include <bits/stdc++.h>
 using namespace std;
 typedef vector<vector<bool>> GridType;
+typedef pair<int,int> PositionType;
+
+static int FPS = 24;
+static int ScreenWidth = 480;
+static int ScreenHeight = 480;
+static int CellSize = 24;
+static int GridSize = ScreenHeight/CellSize;
+
+static void InitGame(void);         // Inicializa o jogo
+static void UpdateGame(void);       // Atualiza um frame (um passo)
+static void DrawGame(void);         // Draw game (one frame)
+static void UnloadGame(void);       // Unload game
+static void UpdateDrawFrame(void);  // Update and Draw (one frame)
+
+static void DrawGrid();
+
+PositionType RetornarPosMouse();
 
 struct Grid {
     GridType Alive;
-    int GridSize;
 
-    Grid (int n){
-        GridSize = n;
-        Alive.resize(n);
-        for (int i = 0; i < n; i++) Alive.at(i).resize(n);
+    Grid (){
+        Alive.resize(GridSize);
+        for (int i = 0; i < GridSize; i++) Alive.at(i).resize(GridSize);
     }
 
     // Executa um passo
@@ -17,28 +33,12 @@ struct Grid {
         GridType buffer = Alive;
         for (int posX = 0; posX < GridSize; posX++){
             for (int posY = 0; posY < GridSize; posY++){
-                // buffer = Rule1(buffer, posX, posY);
-                // buffer = Rule2(buffer, posX, posY);
                 buffer = Rule3(buffer, posX, posY);
                 buffer = Rule4(buffer, posX, posY);
             }
         }
         Alive = buffer;
     }
-
-    // Celulas com menos de 2 celulas vizinhas morrem.
-    // GridType Rule1(GridType buffer, int posX, int posY){
-    //     if (Alive.at(posX).at(posY) == false) return buffer;
-    //     if (CheckSurrounds(posX, posY) < 2) buffer.at(posX).at(posY) = false;
-    //     return buffer;
-    // }
-
-    // // Celulas com mais de 3 celulas vizinhas morrem.
-    // GridType Rule2(GridType buffer, int posX, int posY){
-    //     if (Alive.at(posX).at(posY) == false) return buffer;
-    //     if (CheckSurrounds(posX, posY) > 3) buffer.at(posX).at(posY) = false;
-    //     return buffer;
-    // }
 
     // Celulas com 2 ou 3 sobrevivem (perpetuam).
     GridType Rule3(GridType buffer, int posX, int posY){
@@ -151,13 +151,13 @@ struct Grid {
     }
 
     void GridPrint(){
-        for (int i = 0; i < GridSize; i++){
-            for (int j = 0; j < GridSize; j++){
-                if (Alive.at(i).at(j) == false) cout << ". ";
-                else cout << "x ";
-            } cout << "\n";
+        for (int posX = 0; posX < GridSize; posX++){
+            for (int posY = 0; posY < GridSize; posY++){
+                if (Alive.at(posX).at(posY)) {
+                    DrawRectangle((posX * CellSize), (posY * CellSize), CellSize, CellSize, BLACK);
+                }
+            }
         }
-        cout << "\n";
     }
 
     void Ressurection(pair<int,int> coords){
@@ -169,51 +169,110 @@ struct Grid {
             cout << "A coordenada Y e maior que os limites da grid" << "\n";
             return;
         }
-        Alive.at(coords.first).at(coords.second) = true;
+        Alive.at(coords.first).at(coords.second) = !Alive.at(coords.first).at(coords.second);
     }
 };
 
 int main (){
 
-    Grid gridpadrao(10);
-    int choice;
-    int x,y;
-    while (1) {
-
-        cout << "[0] - Exit" << "\n";
-        cout << "[1] - Add Point" << "\n";
-        cout << "[2] - Show Grid" << "\n";
-        cout << "[3] - Check Surrounds" << "\n";
-        cout << "[4] - Run One Step" << "\n";
-        cin >> choice;
-        switch (choice){
-            case 0:
-                return 0;
-                break;
-
-            case 1:
-                cout << "type (X, Y)" << "\n";
-                cin >> x >> y;
-                gridpadrao.Ressurection({x,y});
-                break;
-            
-            case 2:
-                gridpadrao.GridPrint();
-                break;
-
-            case 3:
-                cout << "type (X, Y)" << "\n";
-                cin >> x >> y;
-                gridpadrao.CheckSurrounds(x,y);
-                break;
-
-            case 4:
-                gridpadrao.Exec();
-                break;
-
-            default:
-                return 0;
-        }        
-    }
+    InitWindow(ScreenWidth, ScreenHeight, "Jogo da Vida");
+    InitGame();
+    
     return 0;
+}
+
+static void InitGame(void){
+
+    Grid GridPadrao;
+    SetTargetFPS(FPS);
+    bool GameIsPaused = false;
+    while (!WindowShouldClose()){
+        // PROCESS EVENTS
+        
+        if (IsKeyPressed(KEY_SPACE)){
+            GameIsPaused = !GameIsPaused;
+        }
+
+        if (IsKeyPressed(KEY_KP_7)){
+            GridPadrao.Exec();
+        }
+
+    
+        PositionType MousePos = RetornarPosMouse();
+        if ((GameIsPaused) && (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))){
+            GridPadrao.Ressurection(MousePos);
+        }
+        // UPDATE GAME LOGIC
+
+        // RENDER
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawGrid();
+        if (GameIsPaused) DrawText("Game Is Paused", 90, 10, 20, BLACK);
+
+        GridPadrao.GridPrint();
+
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+    // int choice;
+    // int x,y;
+    // while (1) {
+
+    //     cout << "[0] - Exit" << "\n";
+    //     cout << "[1] - Add/Remove Point" << "\n";
+    //     cout << "[2] - Show Grid" << "\n";
+    //     cout << "[3] - Check Surrounds" << "\n";
+    //     cout << "[4] - Run One Step" << "\n";
+    //     cin >> choice;
+    //     switch (choice){
+    //         case 0:
+    //             return;
+    //             break;
+
+    //         case 1:
+    //             cout << "type (X, Y)" << "\n";
+    //             cin >> x >> y;
+    //             GridPadrao.Ressurection({x,y});
+    //             break;
+            
+    //         case 2:
+    //             GridPadrao.GridPrint();
+    //             break;
+
+    //         case 3:
+    //             cout << "type (X, Y)" << "\n";
+    //             cin >> x >> y;
+    //             GridPadrao.CheckSurrounds(x,y);
+    //             break;
+
+    //         case 4:
+    //             GridPadrao.Exec();
+    //             break;
+
+    //         default:
+    //             return;
+    //     }        
+    // }
+}
+
+static void UpdateGame(void);       // Atualiza um frame (um passo)
+static void DrawGame(void);         // Draw game (one frame)
+static void UnloadGame(void);       // Unload game
+static void UpdateDrawFrame(void);  // Update and Draw (one frame)
+
+static void DrawGrid(){
+    for (int i = 0; i < GridSize; i++){
+        DrawRectangle((i*CellSize), 0, 1, ScreenHeight, BLACK);
+        DrawRectangle(0, (i*CellSize), ScreenWidth, 1, BLACK);
+    }
+}
+
+PositionType RetornarPosMouse(){
+    
+    Vector2 MousePosition = GetMousePosition();
+    int mouseX = (MousePosition.x / CellSize);
+    int mouseY = (MousePosition.y / CellSize); 
+
+    return make_pair(mouseX, mouseY);
 }
